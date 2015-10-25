145a044381ceb8e7877f6203b7ce72d632f59055
Jahanzeb2199 <k112199@nu.edu.pk>
2015-10-25 15:32:46 +0500

    AlphaCompare new project

--- Google_search/googleProject/googleProject/src/GoogleProject.java
+++ Google_search/googleProject/googleProject/src/GoogleProject.java
@@ -2,22 +2,16 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 //import javax.lang.model.element.Element;
 //import javax.lang.model.util.Elements;
 import org.jsoup.Jsoup;
@@ -27,12 +21,9 @@ import org.jsoup.select.Elements;
 
 //import org.apache.commons.io.FileUtils;
 //import org.apache.commons.lang3.ArrayUtils;
-
-
 public class GoogleProject {
 
-    
- public static ArrayList<String> sendList(String searchString) throws IOException {
+    public static ArrayList<String> sendList(String searchString) throws IOException {
         ArrayList<String> results = new ArrayList<>();
         //<editor-fold defaultstate="collapsed" desc="initiliaize google bot for searching">
         String google = "http://www.google.com/search?q=";
@@ -40,7 +31,7 @@ public class GoogleProject {
         String userAgent = "ExampleBot 1.0 (+http://example.com/bot)"; // Change this to your company's name and bot homepage!
         String url;
         //System.out.println(google + URLEncoder.encode(searchString, charset));
-       // System.setProperty("http.proxyHost", "10.2.20.18");
+        // System.setProperty("http.proxyHost", "10.2.20.18");
         //System.setProperty("http.proxyPort", "9090");
         Document doc = Jsoup.connect(google + URLEncoder.encode(searchString, charset)).userAgent(userAgent).get();
         // System.out.println(doc);
@@ -61,7 +52,7 @@ public class GoogleProject {
         return results;
     }
 
- public static String join(Collection<String> s, String delimiter) {
+    public static String join(Collection<String> s, String delimiter) {
         StringBuffer buffer = new StringBuffer();
         Iterator<String> iter = s.iterator();
         while (iter.hasNext()) {
@@ -72,78 +63,78 @@ public class GoogleProject {
         }
         return buffer.toString();
     }
- 
- public String readFile(String filename) throws IOException
-{
-    String content = null;
-    File file = new File(filename); //for ex foo.txt
-    FileReader reader = null;
-    try {
-        reader = new FileReader(file);
-        char[] chars = new char[(int) file.length()];
-        reader.read(chars);
-        content = new String(chars);
-        reader.close();
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        if(reader !=null){reader.close();}
+
+    public String readFile(String filename) throws IOException {
+        String content = null;
+        File file = new File(filename); //for ex foo.txt
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
+            char[] chars = new char[(int) file.length()];
+            reader.read(chars);
+            content = new String(chars);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return content;
     }
-    return content;
-}
-public String getStringTripple(String a){      
-    
-    String s[]=a.split(" ");
+
+    public String getStringTripple(String a) {
+
+        String s[] = a.split(" ");
         List<String> asList = Arrays.asList(s);
         //Set<String> mySet = new HashSet<String>(asList);
-         Map<String, Integer> unique = new LinkedHashMap<String, Integer>();
-        String string="";
-        int count=0;
-        for(int i=0;i<asList.size();i++){
-           for(int j=i;j<s.length;j++){
-               count++;
-               if(count>7){
-                 break;
-             }
-               string+= asList.get(j);
-             if(unique.get(string) == null)
-                unique.put(string, 1);
-            else
-                unique.put(string, unique.get(string) + 1);
+        Map<String, Integer> unique = new LinkedHashMap<String, Integer>();
+        String string = "";
+        int count = 0;
+        for (int i = 0; i < asList.size(); i++) {
+            for (int j = i; j < s.length; j++) {
+                count++;
+                if (count > 7) {
+                    break;
+                }
+                string += asList.get(j);
+                if (unique.get(string) == null) {
+                    unique.put(string, 1);
+                } else {
+                    unique.put(string, unique.get(string) + 1);
+                }
                //System.out.println(asList.get(i));
-             
-           }
-           count=0;
-           string="";
+
+            }
+            count = 0;
+            string = "";
         }
         String uniqueString = join(unique.keySet(), ", ");
         List<Integer> value = new ArrayList<Integer>(unique.values());
         // Get a set of the entries
-      Set set = unique.entrySet();
-      // Get an iterator
-      Iterator i = set.iterator();
-      // Display elements
-      String max="";
-      while(i.hasNext()) {
-         Map.Entry me = (Map.Entry)i.next();
-        // System.out.print(me.getKey() + ": ");
-        // System.out.println(me.getValue());
-      }
-      int maxValueInMap=(Collections.max(unique.values()));  // This will return max value in the Hashmap
-      System.out.println(maxValueInMap);
+        Set set = unique.entrySet();
+        // Get an iterator
+        Iterator i = set.iterator();
+        // Display elements
+        String max = "";
+        while (i.hasNext()) {
+            Map.Entry me = (Map.Entry) i.next();
+         System.out.print(me.getKey() + ": ");
+             System.out.println(me.getValue());
+        }
+        int maxValueInMap = (Collections.max(unique.values()));  // This will return max value in the Hashmap
+        System.out.println(maxValueInMap);
         for (Map.Entry<String, Integer> entry : unique.entrySet()) {  // Itrate through hashmap
-            if (entry.getValue()==maxValueInMap) {
+            if (entry.getValue() == maxValueInMap) {
                 System.out.println(entry.getKey());     // Print the key with max value
-                max= entry.getKey();
+                max = entry.getKey();
             }
         }
-        
-        
 
-         
        // System.out.println("Output = " + uniqueString);
-  //System.out.println("Values = " + value);
-     return max;
-}
+        //System.out.println("Values = " + value);
+        return max;
+    }
 
 }
